e934b77b20ef52207cde883ff7842b82a14aefc6
Shueny <shuen29@gmail.com>
2019-04-22 18:33:49 +0800

    deploy

---
+++ precache-manifest.57da7ca1bdafed8472f4eb584eb561b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac97299a92a18b7b0654",
+    "url": "/side-projects/static/js/about.6599d58c.js"
+  },
+  {
+    "revision": "615ec185d528c464619e",
+    "url": "/side-projects/static/css/app.af979c8c.css"
+  },
+  {
+    "revision": "615ec185d528c464619e",
+    "url": "/side-projects/static/js/app.a3d94d1e.js"
+  },
+  {
+    "revision": "b24b9d3aa8ad4acfce8a",
+    "url": "/side-projects/static/css/todolist.95880488.css"
+  },
+  {
+    "revision": "b24b9d3aa8ad4acfce8a",
+    "url": "/side-projects/static/js/todolist.f62270ed.js"
+  },
+  {
+    "revision": "96dd5fa273ac0a0e0d1b",
+    "url": "/side-projects/static/js/vendors~todolist.d2b619cf.js"
+  },
+  {
+    "revision": "8b8800e803fc813dc80d0b20fbe2cb07",
+    "url": "/side-projects/static/img/vue_logo.8b8800e8.png"
+  },
+  {
+    "revision": "0dac59f80fceadc556511d45b4632e9c",
+    "url": "/side-projects/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/side-projects/robots.txt"
+  }
+];
