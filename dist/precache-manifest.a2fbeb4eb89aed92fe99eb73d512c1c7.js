168bd86c8c1afacb22ca1dcae632bde09fc91976
Shueny <shuen29@gmail.com>
2019-04-22 16:24:35 +0800

    remove /dist from git-ignore

---
+++ dist/precache-manifest.a2fbeb4eb89aed92fe99eb73d512c1c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "38a47e4707e2a71e97e8",
+    "url": "/vue-sideprojects/static/js/about.bd87ddbc.js"
+  },
+  {
+    "revision": "fe0f9c580416a19cf658",
+    "url": "/vue-sideprojects/static/css/app.af979c8c.css"
+  },
+  {
+    "revision": "fe0f9c580416a19cf658",
+    "url": "/vue-sideprojects/static/js/app.7123c02e.js"
+  },
+  {
+    "revision": "2d97e4a9a2cf0b5c9872",
+    "url": "/vue-sideprojects/static/css/todolist.95880488.css"
+  },
+  {
+    "revision": "2d97e4a9a2cf0b5c9872",
+    "url": "/vue-sideprojects/static/js/todolist.b44599a4.js"
+  },
+  {
+    "revision": "83060a826befb11a7d15",
+    "url": "/vue-sideprojects/static/js/vendors~todolist.4e66f601.js"
+  },
+  {
+    "revision": "8b8800e803fc813dc80d0b20fbe2cb07",
+    "url": "/vue-sideprojects/static/img/vue_logo.8b8800e8.png"
+  },
+  {
+    "revision": "96e7e17e28c58bd027740539a1137893",
+    "url": "/vue-sideprojects/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-sideprojects/robots.txt"
+  }
+];
